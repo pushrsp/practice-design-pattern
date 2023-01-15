3b1fe6385ede0ea307627b84376831aca2b77dc3
PUSHRSP <pushrsp@gmail.com>
2023-01-15 13:59:31 +0900

    #3 - 데코레이터 패턴

--- src/main/java/com/example/practicespring/PracticeSpringApplication.java
+++ src/main/java/com/example/practicespring/PracticeSpringApplication.java
@@ -61,6 +61,20 @@ public class PracticeSpringApplication {
         aopBrowser.show();
         System.out.println("loading time: " + end.get());
         */
+
+        /* 데코레이터
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice();
+
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice();
+
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice();
+        */
     }
 
     public static void connect(Electronic110V electronic110V) {
---
+++ src/main/java/com/example/practicespring/decorator/Audi.java
@@ -0,0 +1,19 @@
+package com.example.practicespring.decorator;
+
+public class Audi implements ICar {
+    private int price;
+
+    public Audi(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void showPrice() {
+        System.out.println("audi의 가격은 " + this.price);
+    }
+}
